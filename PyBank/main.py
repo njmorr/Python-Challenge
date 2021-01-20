115c76d434edc52fc15c32a67b0d6bced8e121d5
njmorr <73321927+njmorr@users.noreply.github.com>
2021-01-19 23:08:51 -0600

    added average change, greateat increase, greatest decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,20 @@ import os
 
 import csv
 
+import operator
+# will be used below to find greatest increaes and decrease on a monthly basis
+# https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+# Accessed 19 Jan 2021
+
 csvpath = os.path.join('Resources','budget_data.csv')
+
+# create empty lists
+month = []
+ledger = []
+delta = []
+deltaMod = []
+
+deltaDict = dict()
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -16,19 +29,77 @@ with open(csvpath) as csvfile:
 
     sumOfMonth = 0
     total = 0
-    
-    # first FOR loop
-    for column in csvreader:
-        #print(column[1])
-        ledger = [int(column[1])]      
-        #print(type(ledger))
+    greatestIncrease = 0
+    greatestDecrease = 0
+    sumOfDelta = 0
 
+    # for loop for cycling through the data set
+    for column in csvreader:
+        month.append(column[0])
+        ledger.append(int(column[1]))
+ 
+    # count number of months in data set
         if ledger != 0:
             sumOfMonth = sumOfMonth + 1
   
-        for PNL in ledger:
-            total = total + PNL
-       
+    # sum up the total profits and losses in data set
+    for PNL in ledger:
+        total = total + PNL
+        
+    # create a version of the delta list that can be modified       
+    for (i, value) in enumerate(ledger):
+        previousPNL = ledger[i-1] if i > 0 else 0
+        currentPNL = value
+        deltaPNL = currentPNL - previousPNL
+        deltaMod.append(int(deltaPNL))
+    
+    del deltaMod[0]
+    # print(deltaMod)
+    countOfChange = len(deltaMod)
+    
+    for monthlyDelta in deltaMod:
+        sumOfDelta = sumOfDelta + monthlyDelta
+    
+   
+    # find greatest increase month-to-month         
+    for (i, value) in enumerate(ledger):
+        previousPNL = ledger[i-1] if i > 0 else 0
+        currentPNL = value
+        deltaPNL = currentPNL - previousPNL
+        delta.append(int(deltaPNL))
+        # print(delta)
+        # Comparing adjacent values in a list
+        # https://www.quora.com/How-do-I-iterate-through-a-list-in-python-while-comparing-the-values-at-adjacent-indices
+        # Accessed 19 January 2021
+    # print(len(delta))  
+    
+
+    for m in month:
+        for change in delta:
+            deltaDict[m] = change
+            delta.remove(change)
+            break
+        # Converting two lists into a dictionary
+        # https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/
+        # Accessed 19 Jan 2021
+    
+    # print(delta)
+    # print(deltaDict)
+
+    greastestIncreaseMonth = (max(deltaDict.items(), key=operator.itemgetter(1))[0])
+    greastestIncreasePNL = (max(deltaDict.items(), key=operator.itemgetter(1))[1])
+    greastestDecreaseMonth = (min(deltaDict.items(), key=operator.itemgetter(1))[0])
+    greastestDecreasePNL = (min(deltaDict.items(), key=operator.itemgetter(1))[1])
+    # print(greastestDecreaseMonth)
+    # print(greastestDecreasePNL)
+
 
     print(f'Total Months: {sumOfMonth}')
     print(f'Total: ${total}')
+    averageChange = round(sumOfDelta/countOfChange,2)
+    # Rounding to 2 decimals
+    # https://stackoverflow.com/questions/20457038/how-to-round-to-2-decimals-with-python
+    # accessed 19 Jan 2021    
+    print(f'Average Change: {averageChange}')
+    print(f'Greatest Increase in Profits: {greastestIncreaseMonth} (${greastestIncreasePNL})')
+    print(f'Greatest Decrease in Profits: {greastestDecreaseMonth} (${greastestDecreasePNL})')
