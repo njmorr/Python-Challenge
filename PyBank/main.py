9b182bdf642b7d5493ded555d06c310ffd7fe34e
njmorr <73321927+njmorr@users.noreply.github.com>
2021-01-19 19:20:14 -0600

    first commit, Read .csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+# print("Hello World")
+
+import os
+
+import csv
+
+csvpath = os.path.join('Resources','budget_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # first FOR loop
+    for column in csvreader:
+        print(column[1])
