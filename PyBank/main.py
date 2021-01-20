fd9a68ae2b26a4d0ea677e49b0cd59a12b742b1e
njmorr <73321927+njmorr@users.noreply.github.com>
2021-01-19 19:58:22 -0600

    added total months + total PnL

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,11 +9,26 @@ csvpath = os.path.join('Resources','budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
 
-    print(csvreader)
+    #print(csvreader)
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
+    sumOfMonth = 0
+    total = 0
+    
     # first FOR loop
     for column in csvreader:
-        print(column[1])
+        #print(column[1])
+        ledger = [int(column[1])]      
+        #print(type(ledger))
+
+        if ledger != 0:
+            sumOfMonth = sumOfMonth + 1
+  
+        for PNL in ledger:
+            total = total + PNL
+       
+
+    print(f'Total Months: {sumOfMonth}')
+    print(f'Total: ${total}')
